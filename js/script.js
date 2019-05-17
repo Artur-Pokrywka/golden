b52acb35f79246c701f35db019274a7c7053b359
Artur Pokrywka <pokrywkaart@gmail.com>
2019-05-17 20:07:09 +0200

    Add header section

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var callUsButton = document.getElementById('contact-button');
+
+callUsButton.addEventListener('click', displayForm);
+
+
+// czy trn element to prompt czy modal??
+function displayForm() {
+    var contactForm = prompt();
+};
+
+
